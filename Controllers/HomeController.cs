0ba71e75f13b53677ca1b728368f9db96e8baa55
xernai <montcorona@gmail.com>
2020-05-27 20:08:39 -0500

    Initial Commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using CommandRestfulAPI.MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace CommandRestfulAPI.MVC.Controllers
+{
+    /// <summary>
+    /// Controller to connect to API
+    /// </summary>
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<Command> commandList = new List<Command>();
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync("https://localhost:44303/api/commands/"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    commandList = JsonConvert.DeserializeObject<List<Command>>(apiResponse);
+                }
+            }
+            return View(commandList);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteCommand(int commandId)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.DeleteAsync("https://localhost:44303/api/commands/" + commandId))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                }
+            }
+
+            return RedirectToAction("Index");
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
